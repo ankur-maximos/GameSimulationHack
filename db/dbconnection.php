e3bb591c784d13451cc783042a0285233a8a2879
xjbhenry <xjbhenry@gmail.com>
2015-03-28 23:02:47 +0000

    fix db

--- db/dbconnection.php
+++ db/dbconnection.php
@@ -1,11 +1,11 @@
 <?php
-$server = 'tcp:game-simu-edu-server.database.windows.net,1433'; 
-$serverusername = 'hackhack@game-simu-edu-server'; 
-$serverpassword = 'hackPSU!@#';
+$server = "tcp:game-simu-edu-server.database.windows.net,1433"; 
+$user = "hackhack"@game-simu-edu-server; 
+$pwd = "hackPSU!@#";
 $db = 'user';
 
 try{
-    $conn = new PDO( "sqlsrv:Server= $server ; Database = $db ", $serverusername, $serverpassword);
+    $conn = new PDO( "sqlsrv:Server= $server ; Database = $db ", $user, $pwd);
     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 }
 catch(Exception $e){
