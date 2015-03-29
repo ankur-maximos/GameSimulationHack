df4f81687c8179f70947090c7c7999a21c777248
xjbhenry <xjbhenry@gmail.com>
2015-03-29 00:55:37 +0000

    added dbconnection.php

--- db/dbconnection.php
+++ db/dbconnection.php
@@ -1,16 +1,52 @@
 <?php
-/*$server = "tcp:game-simu-edu-server.database.windows.net,1433"; 
-$user = "hackhack@game-simu-edu-server"; 
-$pwd = "hackPSU!@#";
-$db = 'user';*/
-echo "cool";
-try {
-    $conn = new PDO ("sqlsrv:server = tcp:game-simu-edu-server.database.windows.net,1433; Database = game-simu-edu-db", "hackhack", "hackPSU!@#");
-    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+function connect() {
+    try {
+        $conn = new PDO ("sqlsrv:server = tcp:game-simu-edu-server.database.windows.net,1433; Database = game-simu-edu-db", "hackhack", "hackPSU!@#");
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+    catch ( PDOException $e ) {
+        print( "Error connecting to SQL Server." );
+        die(print_r($e));
+    }
+    return $conn;
 }
-catch ( PDOException $e ) {
-    print( "Error connecting to SQL Server." );
-    die(print_r($e));
+
+function markItemComplete($table, $item_id)
+{
+	$conn = connect();
+	$sql = "UPDATE ? SET is_complete = 1 WHERE id = ?";
+	$stmt = $conn->prepare($sql);
+	$stmt->bindValue(1, $table);
+	$stmt->bindValue(2, $item_id);
+	$stmt->execute();
 }
-echo "cool";
+function getAllItems($table)
+{
+	$conn = connect();
+	$sql = "SELECT * FROM ?";
+	$stmt = $conn->prepare($sql);
+	$stmt->bindValue(1, $table);
+	$stmt->execute();
+}
+/*function addItem($name, $category, $date, $is_complete)
+{
+	$conn = connect();
+	$sql = "INSERT INTO items (name, category, date, is_complete) VALUES (?, ?, ?, ?)";
+	$stmt = $conn->prepare($sql);
+	$stmt->bindValue(1, $name);
+	$stmt->bindValue(2, $category);
+	$stmt->bindValue(3, $date);
+	$stmt->bindValue(4, $is_complete);
+	$stmt->execute();
+}*/
+function deleteItem($table, $item_id)
+{
+	$conn = connect();
+	$sql = "DELETE FROM ? WHERE id = ?";
+	$stmt = $conn->prepare($sql);
+	$stmt->bindValue(1, $table);
+	$stmt->bindValue(2, $item_id);
+	$stmt->execute();
+}
+
 ?>
