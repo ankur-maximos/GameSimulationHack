f4fce63e30f98e02e06b2e77931ad492c34c1af2
xjbhenry <xjbhenry@gmail.com>
2015-03-29 07:27:10 +0000

    login

--- db/dboperations.php
+++ db/dboperations.php
@@ -16,18 +16,44 @@ function getAllItems($table) {
 	$result->query($stmt);
 	return $result->fetchAll(PDO::FETCH_NUM);
 }
-/*function addItem($name, $category, $date, $is_complete)
-{
-	$conn = connect();
-	$sql = "INSERT INTO items (name, category, date, is_complete) VALUES (?, ?, ?, ?)";
-	$stmt = $conn->prepare($sql);
-	$stmt->bindValue(1, $name);
-	$stmt->bindValue(2, $category);
-	$stmt->bindValue(3, $date);
-	$stmt->bindValue(4, $is_complete);
-	$stmt->execute();
-}*/
-function deleteItem($table, $item_id {
+function validate_prof($username, $password) {
+    $conn = connect();
+    $sql = "SELECT COUNT(*) FROM [user] WHERE ID = ? AND Password = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindValue(1, $username);
+    $stmt->bindValue(2, $password);
+    try {
+        $stmt->execute();
+    } catch(PDOException $e) {
+        print( "Error " );
+        die(print_r($e));
+    }
+    if ($stmt->fetchColumn() > 0)
+    	return 1;
+    else
+    	return 0;
+}
+
+function validate_class($username, $password, $simId) {
+    $conn = connect();
+    $sql = "SELECT COUNT(*) FROM [simulation_table] WHERE User_ID = ? AND Sim_ID = ? AND Sim_Password = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindValue(1, $username);
+    $stmt->bindValue(2, $simId);
+    $stmt->bindValue(3, $password);
+    try {
+        $stmt->execute();
+    } catch(PDOException $e) {
+        print( "Error " );
+        die(print_r($e));
+    }
+    if ($stmt->fetchColumn() > 0)
+    	return 1;
+    else
+    	return 0;
+}
+
+function deleteItem($table, $item_id) {
 	$conn = connect();
 	$sql = "DELETE FROM ? WHERE id = ?";
 	$stmt = $conn->prepare($sql);
