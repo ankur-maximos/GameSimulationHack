ae8192b96c0a76c39206f9cfeb2beceee7d07c1d
xjbhenry <xjbhenry@gmail.com>
2015-03-29 02:25:03 +0000

    test

--- login.php
+++ login.php
@@ -50,9 +50,9 @@
           <input type="radio" name="radio" id="inlineRadio3" value="3"> Admin
         </label>
         <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
+        <input type="email" id="inputEmail" name="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
         <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+        <input type="password" id="inputPassword" name="inputPassword" class="form-control" placeholder="Password" required="">
         <div class="checkbox">
           <label>
             <input type="checkbox" value="remember-me"> Remember me
