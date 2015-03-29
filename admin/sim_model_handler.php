9559233a42476ee6d8ca61afa090a1c52a1df1cc
xjbhenry <xjbhenry@gmail.com>
2015-03-29 09:28:02 +0000

    admin

--- admin/sim_model_handler.php
+++ admin/sim_model_handler.php
@@ -1,22 +1,51 @@
 <?php
 include("../db/newdbconn.php");
-if(isset($_POST['radio'])) {
-    $type = $_POST['radio'];
+if(isset($_POST['name-field'])) {
+    $name_field = $_POST['name-field'];
 }
-if(isset($_POST['radio'])) {
-    $type = $_POST['radio'];
+if(isset($_POST['people-field'])) {
+    $people_field = $_POST['people-field'];
 }
-if(isset($_POST['radio'])) {
-    $type = $_POST['radio'];
+if(isset($_POST['start-field'])) {
+    $start_field = $_POST['start-field'];
 }
-if(isset($_POST['radio'])) {
-    $type = $_POST['radio'];
-}if(isset($_POST['radio'])) {
-    $type = $_POST['radio'];
+if(isset($_POST['end-field'])) {
+    $start_field = $_POST['end-field'];
 }
-if(isset($_POST['radio'])) {
-    $type = $_POST['radio'];
+if(isset($_POST['step-field'])) {
+    $start_field = $_POST['step-field'];
 }
+
+$decision_num;
+if(isset($_POST['decision-num'])) {
+    $decision_num  = $_POST['decision-num'];
+}
+
+for ($i = 1; $i < $decision_num + 1; $i++) {
+    $dnf = "decision-name-field" . $i;
+    $decision_name_field[$i] = $_POST[$dnf];
+    $dtf = "decision-type-field" . $i;
+    $decision_type_field[$i] = $_POST[$dtf];
+}
+
+$variable_num;
+if(isset($_POST['variable-num'])) {
+    $variable_num  = $_POST['variable-num'];
+}
+
+for ($i = 1; $i < $variable_num + 1; $i++) {
+    $dnf = "variable-name-field" . $i;
+    $variable_name_field[$i] = $_POST[$dnf];
+    $dtf = "variable-equation-field" . $i;
+    $variable_equation_field[$i] = $_POST[$dtf];
+}
+
+
+foreach ($variable_name_field as $vnf) {
+    print($vnf);
+}
+
+
 
 
 ?>
