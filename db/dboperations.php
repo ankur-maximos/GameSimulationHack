e5eadcc6cc1cbef239c2114988e3b0c3803ee3c2
xjbhenry <xjbhenry@gmail.com>
2015-03-29 01:00:36 +0000

    added dboperations.php

---
+++ db/dboperations.php
@@ -0,0 +1,39 @@
+<?php
+function markItemComplete($table, $item_id)
+{
+	$conn = connect();
+	$sql = "UPDATE ? SET is_complete = 1 WHERE id = ?";
+	$stmt = $conn->prepare($sql);
+	$stmt->bindValue(1, $table);
+	$stmt->bindValue(2, $item_id);
+	$stmt->execute();
+}
+function getAllItems($table)
+{
+	$conn = connect();
+	$sql = "SELECT * FROM ?";
+	$stmt = $conn->prepare($sql);
+	$stmt->bindValue(1, $table);
+	$stmt->execute();
+}
+/*function addItem($name, $category, $date, $is_complete)
+{
+	$conn = connect();
+	$sql = "INSERT INTO items (name, category, date, is_complete) VALUES (?, ?, ?, ?)";
+	$stmt = $conn->prepare($sql);
+	$stmt->bindValue(1, $name);
+	$stmt->bindValue(2, $category);
+	$stmt->bindValue(3, $date);
+	$stmt->bindValue(4, $is_complete);
+	$stmt->execute();
+}*/
+function deleteItem($table, $item_id)
+{
+	$conn = connect();
+	$sql = "DELETE FROM ? WHERE id = ?";
+	$stmt = $conn->prepare($sql);
+	$stmt->bindValue(1, $table);
+	$stmt->bindValue(2, $item_id);
+	$stmt->execute();
+}
+?>
