2cdf7f4ca6007271e185ae237e740c1a1b2a551c
Swtdream <yingshuo.chen@gmail.com>
2015-03-28 22:39:43 -0400

    admin modified

--- js/admin.js
+++ js/admin.js
@@ -3,6 +3,24 @@ var decisionNum = 0;
 
 $(document).ready(function(){
 
+
+    $('#create-btn').click(function(event) {
+	event.preventDefault();
+	$('#admin-welcome').toggleClass('col-md-12 col-md-0');
+	$('#create-form').toggleClass('col-md-0 col-md-12');
+	$('#admin-welcome').addClass('invisible');
+	});
+	
+	$('.next-btn').click(function(event) {
+	event.preventDefault();
+	var last = $("fieldset .col-md-12:eq(0)");
+	var next = $("fieldset .col-md-0:eq(0)");
+    next.toggleClass('col-md-0 col-md-12');
+	next.removeClass('invisible');
+	last.toggleClass('col-md-12 col-md-00');
+	last.addClass('invisible');
+	
+	});
 	$('.add-btn').click(function(event) {
 	event.preventDefault();
 	if($(this).hasClass('decision-add')) {
@@ -17,8 +35,8 @@ $(document).ready(function(){
     $('.delete-btn').click(function(event) {
 	event.preventDefault();
 	if($(this).hasClass('decision-delete')) {
-	        $(".decision-form .decision-info:last").remove();
-	        $(".decision-form .decision-label:last").remove();			
+	        $("#decision-form .decision-info:last").remove();
+	        $("#decision-form .decision-label:last").remove();			
 			decisionNum = decisionNum - 1;	
 //			$(".decision-form .visible:last").toggleClass('invisible visible');
 			
