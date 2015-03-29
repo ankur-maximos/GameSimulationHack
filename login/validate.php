7fc10da693735ab399dc52f92673931c8b9f877c
xjbhenry <xjbhenry@gmail.com>
2015-03-29 16:14:36 +0000

    admin

--- login/validate.php
+++ login/validate.php
@@ -22,7 +22,7 @@ if(isset($_POST['inputSimId'])) {
 if ($type == 2 && validate_prof($email, $password) == 1) {
     $_SESSION["email"] = $email;
     header('Location: ../admin.php');
-    
+    exit();
 }
 
 if ($type == 1 && validate_class($email, $password, $simId) == 1) {
