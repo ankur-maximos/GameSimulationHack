e15cac955bbf12263f9b83c28caf5ed48dbaad8f
xjbhenry <xjbhenry@gmail.com>
2015-03-29 09:32:29 +0000

    danteng

--- admin/sim_model_handler.php
+++ admin/sim_model_handler.php
@@ -25,9 +25,9 @@ if(isset($_POST['decision-num'])) {
 
 for ($i = 1; $i < $decision_num + 1; $i++) {
     $dnf = "decision-name-field" . $i;
-    $decision_name_field[$i] = $_POST[$dnf];
+    $decision_name_field[$i - 1] = $_POST[$dnf];
     $dtf = "decision-type-field" . $i;
-    $decision_type_field[$i] = $_POST[$dtf];
+    $decision_type_field[$i - 1] = $_POST[$dtf];
 }
 
 $variable_num;
@@ -37,12 +37,12 @@ if(isset($_POST['variable-num'])) {
 
 for ($i = 1; $i < $variable_num + 1; $i++) {
     $dnf = "variable-name-field" . $i;
-    $variable_name_field[$i] = $_POST[$dnf];
+    $variable_name_field[$i - 1] = $_POST[$dnf];
     $dtf = "variable-equation-field" . $i;
-    $variable_equation_field[$i] = $_POST[$dtf];
+    $variable_equation_field[$i - 1] = $_POST[$dtf];
 }
 
-
+echo $variable_equation_field[0];
 foreach ($variable_name_field as $vnf) {
     print($vnf);
 }
