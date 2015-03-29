ad9d31bfae3a3d66561551d1b5d4412a559f05f6
xjbhenry <xjbhenry@gmail.com>
2015-03-29 02:06:27 +0000

    test

--- login/validate.php
+++ login/validate.php
@@ -5,17 +5,20 @@ $type = 0;
 $email;
 $password;
 $active;
-
+echo "ok";
 if (isset($_POST['submit'])) {
     if(isset($_POST['radio'])) {
         $type = $_POST['radio'];
     }
+    echo "ok1";
     if(isset($_POST['inputEmail'])) {
         $email = $_POST['inputEmail'];
     }
+    echo "ok2";
     if(isset($_POST['inputPassword'])) {
         $password = $_POST['inputPassword'];
     }
+    echo "ok3";
 }
 if (validate($email, $password, $type) == 1) {
     $_SESSION["email"] = $email;
